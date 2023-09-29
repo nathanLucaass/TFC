3f44e8c0698d5e65530e7df26c91429c00eb466e
Nathan <nathanlucaass.silva@gmail.com>
2023-09-29 12:34:40 -0300

    Created Teams table and Teams model

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,32 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class Teams extends Model<InferAttributes<Teams>,
+InferCreationAttributes<Teams>> {
+  declare id: CreationOptional<number>;
+
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Teams',
+  timestamps: false,
+});
