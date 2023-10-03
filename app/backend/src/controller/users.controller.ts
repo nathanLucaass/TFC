c56ca6495d30660442668b1f44112bc54402a749
Nathan <nathanlucaass.silva@gmail.com>
2023-10-03 16:32:15 -0300

    fix(login): change status 404 to 400

--- app/backend/src/controller/users.controller.ts
+++ app/backend/src/controller/users.controller.ts
@@ -4,7 +4,7 @@ import { loginService } from '../service/users.services';
 const login = async (req: Request, res: Response): Promise<void | Response> => {
   const { email, password } = req.body;
   const response = await loginService(email, password);
-  if (response.status === 'ERROR') return res.status(404).json({ message: response.message });
+  if (response.status === 'ERROR') return res.status(400).json({ message: response.message });
   res.status(200).json(response.data);
 };
 
