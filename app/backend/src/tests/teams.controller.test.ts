5c44f460d0154ef106c8fb13eca5b7b9d5d86425
Nathan <nathanlucaass.silva@gmail.com>
2023-10-03 14:48:39 -0300

    tests refactored

--- app/backend/src/tests/teams.controller.test.ts
+++ app/backend/src/tests/teams.controller.test.ts
@@ -11,60 +11,9 @@ import TeamsModel from "../database/models/TeamsModel";
 import * as teamsService from "../service/teams.services";
 import { getTeamById } from '../controller/teams.controller';
 
-
 chai.use(chaiHttp);
 
 const { expect } = chai;
-
-
-
-describe("Teams Service", () => {
-  beforeEach(function () { sinon.restore(); });
-  it("should return all teams", async () => {
-
-    const teams = [
-      {
-        id: 1,
-        teamName: "Atlético Mineiro",
-      },
-      {
-        id: 6,
-        teamName: "Cruzeiro",
-      },
-    ];
-    sinon.stub(TeamsModel, "findAll").resolves(TeamsModel.bulkBuild(teams));
-    
-    const result = await teamsService.getAllTeamsService();
-
-    expect(result).to.be.deep.eq(teams);
-  });
-
-  it("should return team by id", async () => {
-    const team = {
-      id: 1,
-      teamName: "Cruzeiro",
-    };
-    sinon.stub(TeamsModel, "findByPk").resolves(TeamsModel.build(team));
-
-    const result = await teamsService.getTeamByIdService(1);
-
-    expect(result).to.be.deep.eq({ status: "SUCCESS", data: team });
-  });
-
-  // it("should return a error if id is not provided", async () => {
-  //   const result = await teamsService.getTeamByIdService(null);
-
-  //   expect(result).to.be.deep.eq({ status: "ERROR", message: "Id is required" });
-  // });
-
-  it("should return a error if team not found", async () => {
-    sinon.stub(TeamsModel, "findByPk").resolves(null);
-
-    const result = await teamsService.getTeamByIdService(1);
-
-    expect(result).to.be.deep.eq({ status: "ERROR", message: "Team not found" });
-  });
-});
 
 describe("Teams Controller", () => {
   
@@ -111,4 +60,3 @@ describe("Teams Controller", () => {
     expect(body).to.deep.equal({ message: "Team not found" })
   });
 });
-
--- app/backend/src/tests/teams.service.test.ts
+++ app/backend/src/tests/teams.service.test.ts
@@ -3,13 +3,13 @@ import * as chai from 'chai';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import { app } from '../../../app';
+import { app } from '../app';
 
 import { Response } from 'superagent';
 
-import TeamsModel from "../../../database/models/TeamsModel";
-import * as teamsService from "../../../service/teams.services";
-import { getTeamById } from '../../../controller/teams.controller';
+import TeamsModel from "../database/models/TeamsModel";
+import * as teamsService from "../service/teams.services";
+import { getTeamById } from '../controller/teams.controller';
 
 chai.use(chaiHttp);
 
