fa31ae0df545f37abea28bca96bc4eee96ab4eb0
Nathan <nathanlucaass.silva@gmail.com>
2023-10-03 19:25:52 -0300

    fix(loginMiddleware): transfer email and password existense to middleware

--- app/backend/src/middlewares/loginValidator.ts
+++ app/backend/src/middlewares/loginValidator.ts
@@ -2,8 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 
 const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
+
+  if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
+
   const emailRegex = /\S+@\S+\.\S+/;
   if (emailRegex.test(email) && password.length >= 6) return next();
+
   return res.status(400).json({ message: 'Invalid email or password' });
 };
 
--- app/backend/src/service/users.services.ts
+++ app/backend/src/service/users.services.ts
@@ -19,8 +19,6 @@ type UserResponse = {
 };
 
 export const loginService = async (email: string, password: string): Promise<UserResponse> => {
-  if (!email || !password) return { status: 'ERROR', message: 'All fields must be filled' };
-
   const user = await UsersModel.findOne({ where: { email } });
 
   if (!user) return { status: 'ERROR', message: 'User not found' };
