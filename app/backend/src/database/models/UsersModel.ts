10d7e3b8732d18b3dabfce57af16ca04bf6eaca8
Nathan <nathanlucaass.silva@gmail.com>
2023-10-03 11:30:10 -0300

    created table users

---
+++ app/backend/src/database/models/UsersModel.ts
@@ -0,0 +1,49 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+export default class Users extends Model<InferAttributes<Users>,
+InferCreationAttributes<Users>> {
+  declare id: CreationOptional<number>;
+
+  declare username: string;
+
+  declare role: string;
+
+  declare email: string;
+
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'user',
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: DataTypes.STRING,
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
