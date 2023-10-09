120cb5b7fed619138602f394812688000acfee05
Nathan <nathanlucaass.silva@gmail.com>
2023-10-09 17:55:04 -0300

    i am legend

--- app/backend/src/middlewares/MatchValidator.ts
+++ app/backend/src/middlewares/MatchValidator.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import { getAllTeamsService } from '../service/teams.services';
 
-const matchValidator = (req: Request, res: Response, next: NextFunction) => {
+const matchValidator = async (req: Request, res: Response, next: NextFunction) => {
+  const teams = await getAllTeamsService();
   const { homeTeamId, awayTeamId } = req.body;
   if (homeTeamId === awayTeamId) {
     return res.status(422)
       .json({ message: 'It is not possible to create a match with two equal teams' });
   }
-  if (homeTeamId < 16 || awayTeamId < 16) {
+  if (homeTeamId > teams.length || awayTeamId > teams.length) {
     return res.status(404)
       .json({ message: 'There is no team with such id!' });
   }
